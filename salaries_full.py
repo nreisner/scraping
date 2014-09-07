0035cfde8566763556bbbd47f4f40eb63910bdc2
nreisner <neil@neilreisner.com>
2014-09-07 12:01:48 -0400

    Adding salarys_full file

---
+++ salaries_full.py
@@ -0,0 +1,51 @@
+import csv
+from mechanize import Browser
+from BeautifulSoup import BeautifulSoup
+
+NUMBER_OF_PAGES = 4
+
+#######################GET URL, FILL OUT FORM#########################
+
+url = 'http://mapyourtaxes.mo.gov/MAP/Employees/Employee/SearchEmployees.aspx'
+
+br = Browser()
+br.open(url)
+
+br.select_form('ctl01')
+
+br.form['SearchEmployees1$CalendarYear1$ddlCalendarYear'] = ['2013']
+br.form['SearchEmployees1$ddlAgencies'] = ['931']
+br.form['SearchEmployees1$txtLastName'] = '%'
+br.form['SearchEmployees1$txtFirstName'] = '%'
+
+br.submit()
+
+output = []
+
+for page in range(NUMBER_OF_PAGES):
+	
+	##########This IS THE PART THAT CHANGES THE PAGE################
+	br.select_form('ctl01')
+
+	br.form['MozillaPager1$ddlPageNumber'] = [str(page)]
+	br.submit('MozillaPager1$btnPageNumber')
+
+	#######################THIS PARSES THE HTML###################
+	html = br.response()
+	soup = BeautifulSoup(html)
+
+
+	results_table = soup.find('table', attrs={'id': 'grdEmployees'})
+	
+	for row in results_table.findAll('tr'):
+		
+		output_row =[]
+
+		for cell in row.findAll('td'):
+			output_row.append(cell.text)
+
+		output.append(output_row)
+
+csv_file = open('salaries.csv', 'w')
+writer = csv.writer(csv_file)
+writer.writerows(output)
